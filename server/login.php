e5bbe98c062adbf552fda712b079d2ee65f309cd
Hrijon <hrijon.chp@gmail.com>
2020-01-28 21:29:58 +0800

    comments and update login

--- server/login.php
+++ server/login.php
@@ -1,15 +1,15 @@
 
 <?php
-
- 	//session start will start the connection to the database where require  calls the php page of the connection code
+ 	//session start will start the connection with database server
 	session_start();
 	require'config.php';
 
-//if error it will throw an error as connect error and die the server
+	//check the connection to the database server
+	if ($con->connect_error){
+	 die("Connection failed: " . $con->connect_error);
+	}
 
-//if ($con->connect_error){
-  //  die("Connection failed: " . $con->connect_error);
-//}
+	//Check the parameter exist or not 
 	if(isset($_POST['login']))
 	{
 		$username= $_POST['username'];
@@ -18,29 +18,26 @@
 		$query="select*from UserInfo WHERE username='$username' AND password='$password'";
 		$query_run = mysqli_query($con,$query);
 
+		//check all rows in database 
 		if(mysqli_num_rows($query_run)>0)
 		{		
-			//valid
-		//	include("#user-panel");
+			//compare the userinputs with database
 			$_SESSION['username']=$username;
 			$_SESSION['password']=$password;
 			$results = mysqli_fetch_array($query_run);
 			echo 'successfully login';
-			//echo($results["email"] . "hello from username");
-
 		}
 		else
 		{
 			//invalid
 			echo'<script type="text/javascript"> alert("Invalid Username or Password")</script>';
-			//include("../webclient/index.php");
+			//include("../index.php");
 		}
+	}//isset end
 
-
-	}
 	else{
             echo'<script type="text/javascript"> alert("Username or Password may not match.. Try again")</script>';
-			include("../webclient/index.php");
+			//include("../index.php");
 	}
 
 //include("../index.html");
